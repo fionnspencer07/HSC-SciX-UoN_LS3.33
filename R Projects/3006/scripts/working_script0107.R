10243fe4570f5f99bae47879d9fe92585675b8ff
fionnspencer07 <fionn.spencer07@gmail.com>
2025-07-01 10:11:16 +1000

    initialise packages

---
+++ R Projects/3006/scripts/working_script0107.R
@@ -0,0 +1,2 @@
+# Initialise Packages
+source("C:/Users/fionn/Newcastle Grammar School/NGS SciX-2025 UoN DMG Cancer Signalling - Bioinformatics and Proteomics/Fionn Git/HSC-SciX-UoN_LS3.33/Custom R Functions and Scripts/init_packages.R")
