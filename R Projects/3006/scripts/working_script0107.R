f2f8cd9f94a4d5022a6c89e86f9a8dcddd40e5c7
fionnspencer07 <fionn.spencer07@gmail.com>
2025-07-02 16:39:07 +1000

    creating rmd

--- R Projects/3006/scripts/working_script0107.R
+++ R Projects/3006/scripts/working_script0107.R
@@ -6,6 +6,7 @@ man_cols <- c("#86b0cc",  "#f3e65d", "#d5c1e7", "#eeb84c", "#82c39e", "#525252",
 names(man_cols) <- c("B_cells", "NK_cells", "monocytes", "T_cells", "neutrophils", "megakaryocytes", "pDCs",
                      "plasma_cells", "progenitor_cells", "NKT_cells")
 
+
 seu <- readRDS("./Data/seu_NKT_blood_mouse_PPK_ONC_harmony_integrated_filtered_v2_20231_20236_annot.rds")
 seu_NKT <- subset(seu, subset = cell_types %in% c("T_cells", "NK_cells", "NKT_cells"))
 seu_NKT_focused <- subset(seu_NKT, subset = treatment %in% c("UT", "Naive", "Sham"))
