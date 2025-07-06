bd3f89b234e9d4b560e445f194a0a60be2741c41
Fionn Spencer <fionn.spencer07@gmail.com>
2025-07-06 02:28:07 +0000

    subsetting seu to only include NK and T Cells

--- R Projects/0507DGE/script.R
+++ R Projects/0507DGE/script.R
@@ -5,5 +5,10 @@ source("/workspaces/HSC-SciX-UoN_LS3.33/Custom R Functions and Scripts/init_pack
 man_cols <- c("#86b0cc",  "#f3e65d", "#d5c1e7", "#eeb84c", "#82c39e", "#525252","#4d9f6b", "#b3939e", "#e76031", "#e9944b")
 names(man_cols) <- c("B_cells", "NK_cells", "monocytes", "T_cells", "neutrophils", "megakaryocytes", "pDCs",
                      "plasma_cells", "progenitor_cells", "NKT_cells")
-
+#importing RDS & Init as a Seurat Object
 seu <- readRDS("/workspaces/HSC-SciX-UoN_LS3.33/Data/seu_NKT_blood_mouse_PPK_ONC_harmony_integrated_filtered_v2_20231_20236_annot.rds")
+
+#Subsetting Seurat Object
+
+## NK, T Cells
+seu_NKT <- subset(seu, subset = cell_types %in% c("T_cells", "NK_cells", "NKT_cells"))
