b158ddaaee80786e84e8ccf26c058e1599547c37
Fionn Spencer <fionn.spencer07@gmail.com>
2025-07-06 02:19:56 +0000

    added custom colours

--- R Projects/0507DGE/script.R
+++ R Projects/0507DGE/script.R
@@ -1,4 +1,8 @@
 #init packages
 source("/workspaces/HSC-SciX-UoN_LS3.33/Custom R Functions and Scripts/init_packages.R")
 
-#
+# Custom colors
+man_cols <- c("#86b0cc",  "#f3e65d", "#d5c1e7", "#eeb84c", "#82c39e", "#525252","#4d9f6b", "#b3939e", "#e76031", "#e9944b")
+names(man_cols) <- c("B_cells", "NK_cells", "monocytes", "T_cells", "neutrophils", "megakaryocytes", "pDCs",
+                     "plasma_cells", "progenitor_cells", "NKT_cells")
+
